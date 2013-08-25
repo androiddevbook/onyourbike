3857885fc597feb1ee8b3325aeeb44c5f865cb0a
Justin Mclean <jmclean@apache.org>
2013-08-25 12:41:27 +1000

    Initial chapter 9

---
+++ On Your Bike - Chapter 9/src/com/androiddevbook/onyourbike/chapter9/services/TimerService.java
@@ -0,0 +1,55 @@
+package com.androiddevbook.onyourbike.chapter9.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.androiddevbook.onyourbike.chapter9.model.TimerState;
+
+public class TimerService extends Service {
+    private static String CLASS_NAME;
+
+    private final IBinder binder;
+    private final TimerState timer;
+
+    public TimerService() {
+        CLASS_NAME = getClass().getName();
+        binder = new TimerBinder();
+        timer = new TimerState();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(CLASS_NAME, "onStartCommand");
+        return START_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(CLASS_NAME, "OnCreate");
+        timer.reset();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(CLASS_NAME, "onDestroy");
+    }
+
+    public TimerState getTimer() {
+        return timer;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public class TimerBinder extends Binder {
+        public TimerService getService() {
+            return TimerService.this;
+        }
+    }
+
+}
